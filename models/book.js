21467979aab340ae76ce7c75378e3b6cd344318b
naren <blazeraze.naren@gmail.com>
2020-07-05 12:50:49 +0545

    mongoose unique validator added;allAuthors configured to return values from DB

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -20,5 +21,7 @@ const bookSchema = new mongoose.Schema({
     },
   ],
 });
+
+bookSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Book', bookSchema);
